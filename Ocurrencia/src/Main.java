56eb26853b928da5d3fec9709d6e99fc92040d35
Anibalmc89 <a.molina01@ufromail.cl>
2015-06-16 18:43:15 -0300

    Nueva prueba

--- Ocurrencia/src/Main.java
+++ Ocurrencia/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Texto t = new Texto("¿Cuánto frío hace?");
+		Texto t = new Texto("¿Cuánto frío hace hoy, cuánto frío hará mañana y cuánto calor hará pasado?");
 		System.out.println(t.ocurrencia());
 	}
 
